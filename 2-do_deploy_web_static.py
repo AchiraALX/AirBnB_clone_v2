cd6f13e5890b83eba07e39dedac7adb0b96e9501
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2023-04-10 03:57:46 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
-# Distributes an archive to the web servers.
-
-import os
-import zipfile
-from fabric.operations import put, run
+"""
+script that distributes archive to webservers
+"""
+import os.path
+from fabric.api import *
+from fabric.operations import run, put, sudo
+env.hosts = ['100.25.177.216', '35.153.226.27']
 
 
 def do_deploy(archive_path):
-    """ Distributes an archive to the web servers """
-
-    if not os.path.isdir("archive_path"):
+    """ deploy """
+    if (os.path.isfile(archive_path) is False):
         return False
 
-    put("archive_path", "/tmp/")
-
-    with zipfile. ():
+    try:
+        new_comp = archive_path.split("/")[-1]
+        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(new_folder))
+        run("sudo tar -xzf /tmp/{} -C {}".
+            format(new_comp, new_folder))
+        run("sudo rm /tmp/{}".format(new_comp))
+        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("sudo rm -rf {}/web_static".format(new_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        return True
+    except:
+        return False
